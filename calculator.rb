f889910d1e83059d5fcd1d7fe232bdaba90720e5
Matt Kirk <matt.22222@hotmail.com>
2018-03-15 10:58:53 -0500

    set 6 functions calc'

--- calculator.rb
+++ calculator.rb
@@ -1,8 +1,28 @@
 
 class Calculator
-	Calculator.add(int, int)
-		result = int + int
+	def Calculator.add(int1, int2)
+		result = int1 + int2
 
+	end
 
+	def Calculator.subtract(int1, int2)
+		result = int1 - int2
+	end
+
+	def Calculator.multiply(int1, int2)
+		result = int1 * int2
+	end
+
+	def Calculator.divide(int1, int2)
+		result = int1 / int2
+	end
+
+	def Calculator.exponent(int1, int2)
+		result = int1 ** int2
+	end
+
+	def Calculator.sq_root(int)
+		result = Math.sqrt(int)
+	end
 
 end
--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -2,16 +2,39 @@ require_relative '../calculator'
 
 describe Calculator do 
 	describe '::add' do
-			it 'should return sum of two integers' do
-				expect(Calculator.add(7,7)).to eq 14
-			end
+		it 'should return sum of two integers' do
+			expect(Calculator.add(7,7)).to eq 14
+		end
 	end	
 
-	# describe '::subtract' do
-	# 	it 'should return subtraction of two integers' do
-	# 		expect(Calculator.subtract(14,7)).to eq 7
-	# 	end
-	# end
+	describe '::subtract' do
+		it 'should return subtraction of two integers' do
+			expect(Calculator.subtract(14,7)).to eq 7
+		end
+	end
 
+	describe '::multiply' do
+		it 'should return multiplication of two integers' do
+			expect(Calculator.multiply(2,7)).to eq 14
+		end
+	end
+
+	describe "::divide" do
+		it 'should return division of two integers' do
+			expect(Calculator.divide(14,2)).to eq 7
+		end
+	end
+
+	describe "::exponent" do
+		it 'should return exponent of two integers' do
+			expect(Calculator.exponent(7,2)).to eq 49
+		end
+	end
+
+	describe "::square_root" do
+		it 'should return the square root of an integer' do
+			expect(Calculator.sq_root(49)).to eq 7
+		end
+	end
 
 end
